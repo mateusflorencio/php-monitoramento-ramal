d23ac4d3e90316bef82bbe8044f4f229e5d6f512
mateus <mateusflorencio@outlook.com.br>
2022-10-19 13:41:36 -0300

    feat: update page 10 seconds

--- js/monitoramento.js
+++ js/monitoramento.js
@@ -1,18 +1,30 @@
-$.ajax({
-    url: "./src/main/index.php",
-    type: "GET",
-    success: function(data){                
-        for(let i in data){
-            $('#cartoes').append(`<div class="${['cartao', 'bg_' + data[i].status].join(' ')}">
-                                <div>${data[i].ramal}</div>
-                                <div>${data[i].username}/${data[i].agente}</div>
-                                <span>Calls: ${data[i].historico}</span>
-                                <span class="${data[i].status} icone-posicao"></span>
-                              </div>`)
+const load = ()=> {
+    $('#wrap_time').append(`<div class='time'>Atualizado às ${new Date().toLocaleTimeString()} - ${new Date().toLocaleDateString()} </div>`)
+    $.ajax({
+        url: "./src/main/index.php",
+        type: "GET",
+        success: function(data){ 
+            for(let i in data){
+                $('#cartoes').append(`<div class="${['cartao', 'bg_' + data[i].status].join(' ')}">
+                                    <div>${data[i].ramal}</div>
+                                    <div>${data[i].username}/${data[i].agente}</div>
+                                    <span>Calls: ${data[i].historico}</span>
+                                    <span class="${data[i].status} icone-posicao"></span>
+                                    </div>`)
+            }
+        },
+        error: function(e){
+            console.log(e)
         }
-        
-    },
-    error: function(e){
-        console.log(e)
-    }
-});
+    });
+}
+
+window.onload = load()
+
+setInterval(()=>{
+    $('#cartoes').empty()
+    $('#wrap_time').empty()
+    load()
+}, 1000 * 10)
+
+
