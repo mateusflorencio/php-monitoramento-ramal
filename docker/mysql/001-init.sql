c1066cf894dcdcc47f42a864b0c6951c5b045eb8
mateus <mateusflorencio@outlook.com.br>
2022-10-18 21:40:05 -0300

    fix: change column name

--- docker/mysql/001-init.sql
+++ docker/mysql/001-init.sql
@@ -1,7 +1,6 @@
 CREATE TABLE ramais (
   ramal VARCHAR(150) NOT NULL UNIQUE,
-  numero VARCHAR(150) NOT NULL,
-  online BOOLEAN DEFAULT FALSE,
+  username VARCHAR(150) NOT NULL,
   status VARCHAR(150) DEFAULT 'indisponivel',
   agente VARCHAR(150) NOT NULL,
   historico INT DEFAULT 0,
